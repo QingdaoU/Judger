d5cfab829ca2b532a1dbb65a485fd6242e454045
virusdefender <qduliyang@outlook.com>
2016-01-13 12:57:47 +0800

    fix python format

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 # coding=utf-8
 from distutils.core import setup, Extension
-setup(name = 'judger', version = '1.0', ext_modules = [Extension('judger', ['judger.c', 'runner.c'])])
+setup(name='judger', version='1.0', ext_modules=[Extension('judger', ['judger.c', 'runner.c'])])
