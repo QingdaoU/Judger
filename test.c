08cd7141c29fb4a73e781cb860b595f053c2f556
virusdefender <qduliyang@outlook.com>
2016-01-15 20:31:28 +0800

    update test.c format

--- test.c
+++ test.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 int main(int argc, char *argv[]) {
     int *a = NULL;
+    int j;
+    printf("start\n");
     // 150M
     int v = 150000000;
-    printf("%s\n", getenv("LD_PRELOAD"));
-    for (int j = 0; j < argc; j++)
+    //fork();
+   // printf("%s\n", getenv("LD_PRELOAD"));
+    for (j = 0; j < argc; j++)
                    printf("argv[%d]: %s\n", j, argv[j]);
     a = (int *) malloc(v);
     if (a == NULL) {
@@ -15,5 +20,6 @@ int main(int argc, char *argv[]) {
         memset(a, 0, v);
         printf("success\n");
     }
+    printf("end\n");
     return 15;
 }
