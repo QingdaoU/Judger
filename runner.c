d2f84abd69fe9b11bcaae0940d754643386e329f
virusdefender <qduliyang@outlook.com>
2016-01-20 15:48:30 +0800

    drop whole exexce rule

--- runner.c
+++ runner.c
@@ -35,8 +35,7 @@ int run(struct config *config, struct result *result) {
     int syscalls_whitelist[] = {SCMP_SYS(read), SCMP_SYS(write), SCMP_SYS(fstat),
                                 SCMP_SYS(mmap), SCMP_SYS(mprotect), SCMP_SYS(munmap), 
                                 SCMP_SYS(open), SCMP_SYS(arch_prctl), SCMP_SYS(brk), 
-                                SCMP_SYS(access), SCMP_SYS(exit_group), SCMP_SYS(execve), 
-                                SCMP_SYS(close)};
+                                SCMP_SYS(access), SCMP_SYS(exit_group), SCMP_SYS(close)};
 
     int syscalls_whitelist_length = sizeof(syscalls_whitelist) / sizeof(int);
     scmp_filter_ctx ctx = NULL;
