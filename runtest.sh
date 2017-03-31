3046fde4b489492211bf07e2ad2bdc7d37bdc5f7
xudianc <xudianc@gmail.com>
2017-03-31 14:02:14 +0800

    替换_BSD_SOURCE (#7)

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,5 @@
+apt-get update
+apt-get install cmake liblua5.3-dev
 rm -rf build && mkdir build && cd build && cmake .. && make && make install
 cd ../bindings/Python && rm -rf build && python setup.py install || exit 1
 cd ../../tests/Python_and_core &&  python test.py
--- src/child.c
+++ src/child.c
@@ -1,4 +1,4 @@
-#define _BSD_SOURCE
+#define _DEFAULT_SOURCE
 #define _POSIX_SOURCE
 #define _GNU_SOURCE
 #include <stdio.h>
