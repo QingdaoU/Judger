817275e13d99c5cb7701f3674502f0b5b0508cad
virusdefender <virusdefender@outlook.com>
2017-05-20 09:55:05 +0800

    add debug level

--- src/logger.h
+++ src/logger.h
@@ -13,7 +13,12 @@ void log_close(FILE *);
 
 void log_write(int level, const char *source_filename, const int line_number, const FILE *log_fp, const char *, ...);
 
+#ifdef JUDGER_DEBUG
 #define LOG_DEBUG(log_fp, x...)   log_write(LOG_LEVEL_DEBUG, __FILE__, __LINE__, log_fp, ##x)
+#else
+#define LOG_DEBUG(log_fp, x...)
+#endif
+
 #define LOG_INFO(log_fp, x...)  log_write(LOG_LEVEL_INFO, __FILE__ __LINE__, log_fp, ##x)
 #define LOG_WARNING(log_fp, x...) log_write(LOG_LEVEL_WARNING, __FILE__, __LINE__, log_fp, ##x)
 #define LOG_FATAL(log_fp, x...)   log_write(LOG_LEVEL_FATAL, __FILE__, __LINE__, log_fp, ##x)
