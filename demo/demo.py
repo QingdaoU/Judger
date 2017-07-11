50da8734d52f1f9c4b00310e49cd54f37a672d03
LiYang <yang.li@chaitin.com>
2017-07-11 12:36:02 +0800

    add demo

---
+++ demo/demo.py
@@ -0,0 +1,27 @@
+import _judger
+import os
+
+if os.system("gcc main.c -o main"):
+    print("compile error")
+    exit(1)
+
+ret = _judger.run(max_cpu_time=1000,
+                  max_real_time=2000,
+                  max_memory=128 * 1024 * 1024,
+                  max_process_number=200,
+                  max_output_size=10000,
+                  max_stack=32 * 1024 * 1024,
+                  # five args above can be _judger.UNLIMITED
+                  exe_path="main",
+                  input_path="1.in",
+                  output_path="/dev/stdout",
+                  error_path="/dev/stderr",
+                  args=[],
+                  # can be empty list
+                  env=[],
+                  log_path="judger.log",
+                  # can be None
+                  seccomp_rule_name="c_cpp",
+                  uid=0,
+                  gid=0)
+print(ret)
---
+++ demo/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    char input[1000];
+    scanf("%s", input);
+    printf("Hello %s\n", input);
+    return 0;
+}
